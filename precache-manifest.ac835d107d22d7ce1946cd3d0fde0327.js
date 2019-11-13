a43842b2558bbbea8ee7dfbb4c37828fa0a89a9b
Axel <axosar@gmail.com>
2019-11-12 20:31:51 -0600

    Updates

---
+++ precache-manifest.ac835d107d22d7ce1946cd3d0fde0327.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b5a54870fcfbb3cc4e7038f256e085",
+    "url": "/qrcode/index.html"
+  },
+  {
+    "revision": "be461367b27c488c8a96",
+    "url": "/qrcode/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "84e215aedfaec1eef75f",
+    "url": "/qrcode/static/css/main.dceed8e2.chunk.css"
+  },
+  {
+    "revision": "be461367b27c488c8a96",
+    "url": "/qrcode/static/js/2.d3588f83.chunk.js"
+  },
+  {
+    "revision": "84e215aedfaec1eef75f",
+    "url": "/qrcode/static/js/main.5009a68c.chunk.js"
+  },
+  {
+    "revision": "9cf7ae6aa5814ce26452",
+    "url": "/qrcode/static/js/runtime-main.27b7f3a2.js"
+  },
+  {
+    "revision": "3314437c1d99632c63d31e58ea886f19",
+    "url": "/qrcode/static/media/logo-habitat.3314437c.png"
+  },
+  {
+    "revision": "15ca9bfd1b968a1f06ec9ddc79032d46",
+    "url": "/qrcode/static/media/logo-imcyc.15ca9bfd.svg"
+  }
+]);
